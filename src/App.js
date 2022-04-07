0efc7df71cdf8eeb4b91ce67210e7019e84a1389
OlhaKuzmyn <olya.kuzmyn@gmail.com>
2022-04-07 16:09:41 +0200

    upd master

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
-import Users from "./components/Users";
-import Posts from "./components/Posts"
-import Comments from "./components/Comments";
+
 
 function App() {
   return (
     <div>
-      <Users/>
-      <hr/>
-      <Posts/>
-      <hr/>
-      <Comments/>
+
     </div>
   );
 }
